e4a22b46cfdbe8345c38b2f27e72dd9f5b392f45
James Smith <james.smith@djalbat.com>
2026-05-30 10:12:17 +0100

    Propagated.

---
+++ test/mathematics/polynomials.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const { levels } = require("necessary");
+
+const { createSuite } = require("../utilities/test");
+
+const { ERROR_LEVEL } = levels;
+
+const name = "polynomials",
+      logLevel = ERROR_LEVEL,
+      projectsDirectoryPath = "../../Mathematics";
+
+describe.only(name, () => {
+  createSuite(name, logLevel, projectsDirectoryPath);
+});
