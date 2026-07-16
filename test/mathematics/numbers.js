b2bb642d16de6d6486201a16bb2ac07fbf28d758
James Smith <james.smith@djalbat.com>
2026-07-16 13:44:45 +0100

    Propagated.

--- test/mathematics/numbers.js
+++ test/mathematics/numbers.js
@@ -4,9 +4,9 @@ const { levels } = require("necessary");
 
 const { createSuite } = require("../utilities/test");
 
-const { INFO_LEVEL } = levels;
+const { ERROR_LEVEL } = levels;
 
-const logLevel = INFO_LEVEL,
+const logLevel = ERROR_LEVEL,
       projectName = "numbers",
       projectsDirectoryPath = "../../Mathematics";
 
