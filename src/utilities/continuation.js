8307c9d453f97766a03502110585d8f5b2d767ea
James Smith <james.smith@djalbat.com>
2026-07-21 15:42:56 +0100

    Propagated.

--- src/utilities/continuation.js
+++ src/utilities/continuation.js
@@ -7,7 +7,7 @@ const { forEach: asynchronousForEach,
         forwardsForEach: asynchronousForwardsForEach,
         backwardsForEach: asynchronousBackwardsForEach } = asynchronousUtilities;
 
-export const { match, filter, reduce, forEach, resolve } = continuationUtilities;
+export const { reduce, forEach, resolve } = continuationUtilities;
 
 export function one(array, callback, ...callerArguments) {
   let success = false;
@@ -133,6 +133,79 @@ export function every(array, callback, ...callerArguments) {
   });
 }
 
+export function match(arrayA, arrayB, callback, ...callerArguments) {
+  const continuation = callerArguments.pop(),
+        context = callerArguments.pop();
+
+  const arrayALength = arrayA.length,
+        arrayBLength = arrayB.length;
+
+  if (arrayALength !== arrayBLength) {
+    const matches = false;
+
+    return continuation(matches, context);
+  }
+
+  let index = -1;
+
+  return every(arrayA, (elementA, ...calleeArguments) => {
+    index++;
+
+    const elementB = arrayB[index];
+
+    const continuation = calleeArguments.pop();
+
+    let context = calleeArguments.pop();
+
+    return callback(elementA, elementB, ...calleeArguments, context, (...callbackArguments) => {
+      const success = callbackArguments.shift();
+
+      context = callbackArguments.shift();
+
+      return continuation(success, context);
+    });
+  }, ...callerArguments, context, (success, context) => {
+    const matches = success;  ///
+
+    return continuation(matches, context);
+  });
+}
+
+export function filter(array, callback, ...callerArguments) {
+  const deletedElements = [];
+
+  const continuation = callerArguments.pop(),
+        context = callerArguments.pop();
+
+  let index = array.length;
+
+  return backwardsForEach(array, (element, ...calleeArguments) => {
+    index--;
+
+    const continuation = calleeArguments.pop();
+
+    let context = calleeArguments.pop();
+
+    return callback(element, ...calleeArguments, context, (...callbackArguments) => {
+      const success = callbackArguments.shift();
+
+      if (!success) {
+        const start = index,  ///
+              deleteCount = 1,
+              deletedElement = array.splice(start, deleteCount).pop();  ///
+
+        deletedElements.unshift(deletedElement);  ///
+      }
+
+      context = callbackArguments.shift();
+
+      return continuation(context);
+    });
+  }, ...callerArguments, context, (context) => {
+    return continuation(deletedElements, context);
+  });
+}
+
 export function extract(array, callback, ...callerArguments) {
   let deletedElement = undefined; ///
 
@@ -224,6 +297,38 @@ export function backwardsEvery(array, callback, ...callerArguments) {
     });
   }, () => {
     return continuation(success, context);
+  });
+}
+
+export function forwardsForEach(array, callback, ...callerArguments) {
+  const continuation = callerArguments.pop();
+
+  let context = callerArguments.pop();
+
+  asynchronousForwardsForEach(array, (element, next, done) => {
+    return callback(element, ...callerArguments, context, (...callbackArguments) => {
+      context = callbackArguments.shift();
+
+      next();
+    });
+  }, () => {
+    return continuation(context);
+  });
+}
+
+export function backwardsForEach(array, callback, ...callerArguments) {
+  const continuation = callerArguments.pop();
+
+  let context = callerArguments.pop();
+
+  asynchronousBackwardsForEach(array, (element, next, done) => {
+    return callback(element, ...callerArguments, context, (...callbackArguments) => {
+      context = callbackArguments.shift();
+
+      next();
+    });
+  }, () => {
+    return continuation(context);
   });
 }
 
