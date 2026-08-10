03b5e7aaa883af5ac8f0c7d006b1d3516a6e1ceb
James Smith <james.smith@djalbat.com>
2026-08-10 19:33:44 +0100

    Rebuilt.

--- test/logic/firstOrderLogic.js
+++ test/logic/firstOrderLogic.js
@@ -10,6 +10,7 @@ const logLevel = ERROR_LEVEL,
       projectName = "first-order-logic",
       projectsDirectoryPath = "../../Logic";
 
-describe(projectName, () => {
+describe.only(projectName, () => {
   createSuite(logLevel, projectName, projectsDirectoryPath);
 });
+
