aec70fb3d2bdf91cc7142ca89f52967250bde04a
James Smith <james.smith@djalbat.com>
2026-09-13 19:09:54 +0100

    Propagated.

--- test/algorithms/operationalTransformations.js
+++ test/algorithms/operationalTransformations.js
@@ -10,6 +10,6 @@ const logLevel = ERROR_LEVEL,
       projectName = "operational-transformations",
       projectsDirectoryPath = "../../Algorithms";
 
-describe.skip(projectName, () => {
+describe(projectName, () => {
   createSuite(logLevel, projectName, projectsDirectoryPath);
 });
